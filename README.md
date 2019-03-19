626c9710a1759f623cd10b9b29215bf72667de3f
Travezripley <theripleys@gmail.com>
2019-03-19 11:22:12 -0700

    updates

---
+++ README.md
@@ -0,0 +1,10 @@
+March 17th 2019
+
+This is my third project in the Node 101 Module at SDCS.
+Here we implement more Express, Morgan and Axios NPM packages into our project.
+
+I'll update you with my progress here.
+
+My brain is seriously broken.
+
+-Travez
--- server/app.js
+++ server/app.js
@@ -1,8 +1,67 @@
+//March 17th, 2019 * Node101-Movie-Finder-Data * San Diego Code School
+
+//Required NPM Packages
+
+// Create Express server framework
 const express = require('express');
-
-
 const app = express();
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+const axios = require('axios');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+
+//const URL = 'http://www.omdbapi.com';
+
+//const path = require('path');
+
+//const cache = {};
+//API Key from OMDb
+//const API_KEY = "49a19b32";
+
+app.get('/', (req, res) => {
+    
+    //encodeURIComponent(
+    let movieTitle = req.query.t;//look up encode
+    let movieId = req.query.i;
+    
+    //console.log(`movieTitle is ${movieTitle}`);
+    
+    if (movieTitle) {
+        axios
+            .get(`http://www.omdbapi.com/?t=${movieTitle}&apikey=8730e0e`)
+            .then(response => {
+                res.status(200).json(response.data);
+                //cache = {'movieID': movieId, 'data': response.data};
+                //handle response variable
+                //console.log(response.data);
+                //console.log(response.body);
+            })
+            .catch(error => {
+                console.log('error', error);
+        });
+
+    } else if (movieId) {        
+        axios
+            .get(`http://www.omdbapi.com/?i=${movieId}&apikey=8730e0e`)
+            .then(response => {
+                res.status(200).json(response.data);
+                //cache = {'movieID': movieId, 'data': response.data};
+                //handle response variable
+                //console.log(response.data);
+                //console.log(response.body);
+            })
+            .catch(error => {
+                console.log('error', error);
+        });
+        // 
+    }
+    // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+    //console.log(movieTitle);
+
+    // console.log(req.query);
+    // res.send('Hi guys!');
+});
 
 module.exports = app;
